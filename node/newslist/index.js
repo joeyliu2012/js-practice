1d02fbd1e98c86da11d3078afffeadebbc0a3822
joeyliu2012 <joeyliu2012@gmail.com>
2019-09-01 21:13:13 +0800

    update newList pagi and detail

--- node/newslist/index.js
+++ node/newslist/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url')
 const path = require('path');
 const fs = require('fs');
 const mime = require('./mime.json');
@@ -7,18 +8,29 @@ let myData = require('./data.json');
 const cheerio = require('cheerio');
 
 let server = http.createServer((req, res) => {
-    if (req.url === '/news') {
+    // console.log(url.parse(req.url));
+    let urlReq = url.parse(req.url);
+    let pathname = urlReq.path;
+    // console.log(urlReq)
+    let p = urlReq.query || 1;
+    if (pathname === '/news?' + p || pathname === '/news') {
+        // console.log(p);
         let newsData = fs.readFileSync('./views/index.html');
         let $ = cheerio.load(newsData);
         let myHtml = '';
-        myData.forEach(v => {
+        let pageSize = 5;
+        let pages = Math.ceil(myData.length / pageSize);
+        console.log(pages);
+        let data = myData.slice(pageSize * (p - 1), pageSize * p);
+        console.log(data);
+        data.forEach(v => {
             myHtml += `<li class="news">
             <a href="javascript:;">
                 <img src="./img/img.png" alt="">
             </a>
             <div>
                 <h3>
-                    <a href="javascript:;">${v.title}</a>
+                    <a href="/detail?${v.id}">${v.title}</a>
                 </h3>
                 <div class="info">
                     <span class="tips"><span>纵火</span><span>韩国</span><span>逮捕</span></span>
@@ -29,16 +41,39 @@ let server = http.createServer((req, res) => {
         </li>`
         });
         $('.news-list').html(myHtml);
+
+        console.log('p', p);
+        p = parseInt(p);
+        let pagi = '';
+        let prev = p <= 1 ? 1 : p - 1;
+        let next = p >= pageSize ? pageSize : p + 1;
+
+        pagi += `<a href="/news?${prev}" class="prev">⌜</a>`;
+        for (let i = 1; i <= pages; i++) {
+            pagi += ` <a href="/news?${i}">${i}</a>`;
+        }
+        pagi += `<a href="/news?${next}" class="next">⌝</a>`;
+
+        $('.pagination').html(pagi);
+
         res.end($.html());
-    } else if (req.url === '/detail') {
+    } else if (pathname === '/detail?' + p || pathname === '/detail') {
         let newsData = fs.readFileSync('./views/detail.html');
-        res.end(newsData);
+        let $ = cheerio.load(newsData);
+        let temp = '';
+        temp += `<h1 class="title">${myData[p - 1].title}</h1>
+            <div class="article-info"> 类型：纵火 时间：${myData[p - 1].addtime}</div>
+            <p class="content">
+                ${myData[p - 1].detail}
+            </p>`
+        $('.text').html(temp);
+        res.end(temp);
     } else {
-        if (req.url !== '/favicon.ico') {
-            let extname = path.extname(req.url);
+        if (pathname !== '/favicon.ico') {
+            let extname = path.extname(pathname);
             console.log('ex', extname);
             res.writeHead(200, { 'Content-type': mime[extname] });
-            let rs = fs.createReadStream("./views/" + req.url);
+            let rs = fs.createReadStream("./views/" + pathname);
             rs.pipe(res);
         }
     }
