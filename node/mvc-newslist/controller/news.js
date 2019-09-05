bb29ba5ab77919e4643addd497028eeea339ad2e
joeyliu2012 <joeyliu2012@gmail.com>
2019-09-05 16:49:54 +0800

    update mvc newslist

--- node/mvc-newslist/controller/news.js
+++ node/mvc-newslist/controller/news.js
@@ -1,11 +1,27 @@
 const newsService = require('../service/news');
 
 module.exports = {
-    showIndex(ctx) {
-        ctx.body = 'hello';
-        // ctx.render('')
+    async showIndex(ctx) {
+        let size = 5;
+        let p = ctx.query.p || 1;
+        let newsData = newsService.getData(size,p);
+        let prev = newsService.getPages(p,size).prev;
+        let next = newsService.getPages(p,size).next;
+        let pages = newsService.getPages(p,size).pages;
+        // ctx.body = 'hello';
+        await ctx.render('news/index.pug', {
+            newsData,
+            prev,
+            next,
+            pages
+        })
     },
-    showDetail(ctx) {
-        ctx.body = 'news detail';
+    async showDetail(ctx) {
+        let id = ctx.query.id || 1;
+        let detailData = newsService.getDetailData(id);
+        // ctx.body = 'news detail';
+        await ctx.render('news/detail.pug', {
+            detailData,
+        })
     }
 }
--- node/mvc-newslist/service/news.js
+++ node/mvc-newslist/service/news.js
@@ -1,7 +1,20 @@
+let newsData = require('../data/data.json'); 
+
 module.exports = {
-    getData () {
-        return {
-            value: "some value..."
-        }
+    getData (size,p) {
+        let formatData = newsData.slice(size*(p-1), p*size);
+
+        return formatData;
+    },
+    getPages(p,size) {
+        p = parseInt(p);
+        let pages = Math.ceil(newsData.length/size);
+        let prev = p<=1?1:p-1;
+        let next = p>=pages?pages:p+1;
+        return {prev,next,pages};
+    },
+    getDetailData(id) {
+        return newsData[id-1];
     }
+
 }
