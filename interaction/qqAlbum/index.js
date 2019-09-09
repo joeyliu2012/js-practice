a385e44d707160043cfc865600560258ca8dd2fb
joeyliu2012 <joeyliu2012@gmail.com>
2019-09-09 23:41:31 +0800

    add login page

--- interaction/qqAlbum/index.js
+++ interaction/qqAlbum/index.js
@@ -4,6 +4,17 @@ const static = require('koa-static');
 const koaBody = require('koa-body');
 const nunjucks = require('koa-nunjucks-2');
 
+let userData = [
+    {
+        username: '123',
+        password: '123'
+    },
+    {
+        username: 'abc',
+        password: 'abc'
+    }
+]
+
 let app = new Koa();
 let router = new Router();
 
@@ -12,13 +23,56 @@ app.use(koaBody({
 }))
 
 app.use(nunjucks({
-    ext:"html",
+    ext: "html",
     path: __dirname + "/views"
 }))
 app.use(static(__dirname + "/static"));
 
-router.get('/',ctx => {
-    ctx.body = 'hello';
+router.get('/', async ctx => {
+    await ctx.render('login');
+})
+
+router.get('/login', async ctx => {
+    await ctx.render('login');
+})
+
+router.post('/login', ctx => {
+    console.log(ctx.request.body);
+    let req =  JSON.parse(ctx.request.body);
+    // ctx.set("Content-Type", "application/json")
+    let res = userData.find(v => {
+        return v.username == req.username && v.password == req.password;
+    })
+    if(res) {
+        ctx.body = {
+            code: 0,
+            info: '登陆成功'
+        }
+        // ctx.redirect('/photo');
+    } else {
+        ctx.body = {
+            code: 1,
+            info: '登陆失败'
+        }
+    }
+})
+
+router.get('/checkUser', ctx => {
+    let user = ctx.query.username;
+    let res = userData.some(item => item.username == user);
+    if (res) {
+        ctx.body = {
+            id: 1,
+            code: 0,
+            info: '用户名正确'
+        }
+    } else {
+        ctx.body = {
+            id: 2,
+            code: 1,
+            info: '用户名错误'
+        }
+    }
 })
 
 router.get('/photo', async ctx => {
@@ -26,5 +80,4 @@ router.get('/photo', async ctx => {
 })
 
 app.use(router.routes());
-
 app.listen(3000);
